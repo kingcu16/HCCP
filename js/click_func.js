29637e7b2db91d3d38aabaea3952ae04348647a9
CopperWang <kingcu16@163.com>
2018-03-22 23:10:23 +0800

    各个点击功能的实现

--- js/click_func.js
+++ js/click_func.js
@@ -1,17 +1,97 @@
 $(function(){
-
-	$('.menu-1').click(function(){
+	document.Flag={
+		'区域':'全国',
+		'地方':'中国',
+		'维度':'统计',
+		'内容':'天气',
+		'Class':'map'
+	};
+	var TimeFlag;
+	$("#tongjiData,#detailData,#choiceData,#quyuChoice,#Class").click(function(){
 		$(this).next().fadeToggle();
-	})
-	var ff=$('.menu-1');
-	for(var i=0;i<ff.length;i++)
-	{
-		var tt=ff[i].getElementsByClassName('menu-2');
-		for(var j=0;j<tt.length;j++)
-		{
-			tt[i].click(function(){
-				
-			})
+	});
+
+	$("#quyu").click(function(){
+		if($(this).find("option:selected").text()=="地方"){
+			$(this).parent().next().fadeToggle();
+			$(this).parent().next().next().fadeToggle();
+			document.Flag['区域']='地方';
+			document.Flag['时间']='2018-02';
 		}
-	}
+		else{
+			document.Flag['区域']='全国';
+			document.Flag['地方']='中国'
+		}
+	});
+	$(":month").click(function(){
+		
+	})
+	$("#sf").click(function(){
+		document.Flag['地方']=$(this).find("option:selected").text();
+	});
+	$("#tongjiData").click(function(){
+		document.Flag['维度']="统计";
+	});
+	$("#detailData").click(function(){
+		document.Flag['维度']="详细";
+	});
+	$("#tongjiData").next().find('li').click(function(){
+		if(this.F)
+		{
+			this.F=false;
+			$(this).parent().find('li').attr('style','');
+			document.Flag['维度']="统计";
+			document.Flag['内容']="";
+		}
+		else{
+			this.F=true;
+			$(this).parent().find('li').attr('style','');
+			$(this).attr('style','background-color: #72bab5;');
+			document.Flag['维度']="统计";
+			document.Flag['内容']=$(this).text();
+		}		
+	});
+	$("#detailData").next().find('li').click(function(){
+
+		if(this.F)
+		{
+			this.F=false;
+			$(this).parent().find('li').attr('style','');
+			document.Flag['维度']="详细";
+			document.Flag['内容']='';
+		}
+		else{
+			this.F=true;
+			$(this).parent().find('li').attr('style','');
+			$(this).attr('style','background-color: #72bab5;');
+			document.Flag['维度']="详细";
+			document.Flag['内容']=$(this).text();
+		}
+	});
+	$("#Class").next().find('li').click(function(){
+		
+		if(this.F)
+		{
+			this.F=false;
+			$(this).parent().find('li').attr('style','');
+			document.Flag['Class']='';
+		}
+		else{
+			this.F=true;
+			$(this).parent().find('li').attr('style','');
+			$(this).attr('style','background-color: #72bab5;');
+			document.Flag['Class']=$(this).attr('id');
+		}
+	});
+	$('#tjiao').click(function(){
+		var Data=DataInit(document.Flag);
+		var title={
+			text:document.Flag['区域']+document.Flag['维度']+document.Flag['内容']+'信息',
+			left: 'center',
+		    textStyle: {
+		            color: '#fff'
+		        }
+		}
+		EchartInit($('#dituContent'),Data,title,document.Flag['Class'],document.Flag);
+	})
 })
