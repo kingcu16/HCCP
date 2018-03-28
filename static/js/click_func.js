0db857075b63024d1c207cc49337a24525a66dfa
CopperWang <kingcu16@163.com>
2018-03-28 23:08:15 +0800

    调整点击事件逻辑

--- static/js/click_func.js
+++ static/js/click_func.js
@@ -7,19 +7,27 @@ $(function(){
 		'Class':'map',
 		'时间':'2018-2'
 	};
+	setTimeout(ClassInit(),0)
 	var TimeFlag;
 	$("#tongjiData,#detailData,#choiceData,#quyuChoice,#Class").click(function(){
 		$(this).next().fadeToggle();
 	});
+	$('li').click(function(){
+		setTimeout(ClassInit(),0);
+	})
 	$('#date').change(function (){
 		document.Flag['时间']=$(this).val();
 	})
 	$("#quyu").click(function(){
 		if($(this).find("option:selected").text()=="地方"){
 			$(this).parent().next().fadeToggle();
-			$(this).parent().next().next().fadeToggle();
+			if(document.Flag['维度']=='详细')
+			{
+				$(this).parent().next().next().fadeToggle();
+			}
 			document.Flag['区域']='地方';
-			document.Flag['时间']='2018-2';
+			document.Flag['地方']=$('#sf').find("option:selected").text();
+			document.Flag['时间']=$('#date').val();
 		}
 		else{
 			document.Flag['区域']='全国';
@@ -47,6 +55,8 @@ $(function(){
 			this.F=true;
 			$(this).parent().find('li').attr('style','');
 			$(this).attr('style','background-color: #72bab5;');
+			$('#detailData').next().find('li').attr('style','');
+			$('#detailData').next().fadeOut();
 			document.Flag['维度']="统计";
 			document.Flag['内容']=$(this).text();
 		}		
@@ -64,6 +74,8 @@ $(function(){
 			this.F=true;
 			$(this).parent().find('li').attr('style','');
 			$(this).attr('style','background-color: #72bab5;');
+			$('#tongjiData').next().find('li').attr('style','');
+			$('#tongjiData').next().fadeOut();
 			document.Flag['维度']="详细";
 			document.Flag['内容']=$(this).text();
 		}
@@ -73,13 +85,13 @@ $(function(){
 		if(this.F)
 		{
 			this.F=false;
-			$(this).parent().find('li').attr('style','');
+			$(this).parent().find('li').removeClass('active');
 			document.Flag['Class']='';
 		}
 		else{
 			this.F=true;
-			$(this).parent().find('li').attr('style','');
-			$(this).attr('style','background-color: #72bab5;');
+			$(this).parent().find('li').removeClass('active');
+			$(this).addClass('active');
 			document.Flag['Class']=$(this).attr('id');
 		}
 	});
@@ -87,13 +99,41 @@ $(function(){
 		var Data=DataInit(document.Flag);
 		//alert(Data);
 		var title={
-			text:document.Flag['区域']+document.Flag['维度']+document.Flag['内容']+'信息',
+			text:document.Flag['区域']+document.Flag['维度']+document.Flag['内容']+'信息-'+document.Flag['地方'],
 			left: 'center',
 		    textStyle: {
 		            color: '#fff'
 		        }
-		}
-
+		};
 		EchartInit(document.getElementById('dituContent'),Data,title,document.Flag['Class'],document.Flag);
 	})
-})
+});
+function ClassInit()
+{
+	if(document.Flag['维度']=='详细')
+		{
+
+			if(document.Flag['内容']=='温度')
+			{
+				$('#line').show();
+				$('#map,#bar,#pie,#radar').hide();
+			}
+			else{
+				$('#map,#line').hide();
+				$('#bar,#pie,#radar').show();
+			}
+		}
+		if(document.Flag['维度']=='统计')
+		{
+			//$('#date').hide();
+			if(document.Flag['内容']=='天气')
+			{
+				$('#map').show();
+				$('#line,#bar,#pie,#radar').hide();
+			}
+			else{
+				$('#map,#line').hide();
+				$('#bar,#pie,#radar').show();	
+			}
+		}
+}
